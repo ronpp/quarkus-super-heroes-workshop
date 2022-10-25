8f30c0e33115cef8663b94fc739e9bebd707f1a0
Ronny Peña <ra.pp.91.15@gmail.com>
2022-10-25 19:37:20 +0200

    Added documentation(OpenAPI) for the rest-villain api and custom init banner

--- super-heroes/rest-villains/src/test/java/io/quarkus/workshop/superheroes/villain/VillainResourceTest.java
+++ super-heroes/rest-villains/src/test/java/io/quarkus/workshop/superheroes/villain/VillainResourceTest.java
@@ -192,6 +192,16 @@ public class VillainResourceTest {
         assertEquals(NB_VILLAINS, villains.size());
     }
 
+    @Test
+    void shouldPingOpenAPI() {
+        given()
+            .header(ACCEPT, JSON)
+            .when().get("/q/openapi")
+            .then()
+            .statusCode(OK.getStatusCode());
+    }
+
+
     private TypeRef<List<Villain>> getVillainTypeRef() {
         return new TypeRef<List<Villain>>() {
             // Kept empty on purpose
